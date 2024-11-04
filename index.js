882dde9cbd65eca23dbb511e210f649b08fdb7cb
leylarustamova <leylatustamova46@gmail.com>
2024-11-04 15:52:34 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// let instrument=['cup' ,'plate', 'spoon','knife','fork']
+// let i=0
+
+// while(i < instrument.length) {
+//     console.log(instrument[i]);
+//     i++
+
+// }
+
+
+// let terevez = ['xiyar', 'pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni']
+
+// let i = 0
+// while (i < terevez.length) {
+//     if (terevez[i][0] == "s")
+//         console.log(terevez[i]);
+//     i++
+// }
+
+
+
+
+
+// let cem=0
+// let eded=1
+// while(eded<=100) {
+//     cem+=eded   /* cem=cem+eded*/
+//     eded++   
+// }
+// console.log(cem);
+
+
+
+
+
+
+
+
+// let levazimat = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq']
+// let i = 0
+// while (i < levazimat.length) {
+//     if (levazimat[i].length == 5) {
+//         console.log(levazimat[i].slice(-1));   
+//     }
+//     else {
+
+        
+//         i++;
+//         continue;
+//     }
+//     i++
+
+// }
+
+
+
+
+
+// let eded=100000
+// while(eded>=1){
+//     if( eded%9999==0 ){
+//         console.log(eded);
+//         break;
+//     }
+//     eded--
+    
+// }
+
+
+
+
+let setir='Men her gun Javascript oyrenirem'
+let newSetir=setir.replace(/[a,i,o,u,e]/g,'')
+console.log(newSetir);
+
+
