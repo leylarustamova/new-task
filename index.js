61c45b6c8fb99c526e3e539912421ad94348a8e5
leylarustamova <leylatustamova46@gmail.com>
2024-11-18 14:06:02 +0400

    first commit

--- index.js
+++ index.js
@@ -1,227 +1,5 @@
-// let instrument=['cup' ,'plate', 'spoon','knife','fork']
-// let i=0
 
-// while(i < instrument.length) {
-//     console.log(instrument[i]);
-//     i++
 
-// }
-
-
-// let terevez = ['xiyar', 'pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni']
-
-// let i = 0
-// while (i < terevez.length) {
-//     if (terevez[i][0] == "s")
-//         console.log(terevez[i]);
-//     i++
-// }
-
-
-
-
-
-// let cem=0
-// let eded=1
-// while(eded<=100) {
-//     cem+=eded   /* cem=cem+eded*/
-//     eded++   
-// }
-// console.log(cem);
-
-
-
-
-
-
-
-
-// let levazimat = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq']
-// let i = 0
-// while (i < levazimat.length) {
-//     if (levazimat[i].length == 5) {
-//         console.log(levazimat[i].slice(-1));   
-//     }
-//     else {
-
-
-//         i++;
-//         continue;
-//     }
-//     i++
-
-// }
-
-
-
-
-
-// let eded=100000
-// while(eded>=1){
-//     if( eded%9999==0 ){
-//         console.log(eded);
-//         break;
-//     }
-//     eded--
-
-// }
-
-
-
-
-// let setir='Men her gun Javascript oyrenirem'
-// let newSetir=setir.replace(/[a,i,o,u,e]/g,'')
-// console.log(newSetir);
-
-
-
-
-// let instrument=['cup' ,'plate', 'spoon','knife','fork']
-// for(let index of instrument){
-//     console.log(index);
-
-// }
-
-
-
-
-
-// // let terevez = ['xiyar', 'pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni']
-// // for (i = 0; i < terevez.length; i++) {
-// //     if (terevez[i][0] == "s")
-// //         console.log(terevez[i]);
-
-// // }
-
-
-
-
-// let cem=0
-// for(eded=1;eded<=100;eded++){
-//     cem+=eded;
-
-// }
-// console.log(cem);
-
-
-
-
-
-// let levazimat = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq']
-// for( i=0;i<levazimat.length;i++ ){
-//     if(levazimat[i].length==5){
-//         console.log(levazimat[i].slice(-1));
-
-//     }
-//     else{
-//         i++;
-//         continue;
-//     }
-
-// }
-
-
-
-
-
-
-// for(eded=100000;eded>1;eded--){
-//     if(eded%9999==0){
-//         console.log(eded);
-
-//         break;
-
-//     }
-// }
-
-
-
-// let setir='Men her gun Javascript oyrenirem'
-// let newSetir=setir.replace(/[a,i,o,u,e]/g,'')
-// console.log(newSetir);
-
-
-/*yeni calismalar--------------------------------------------------------------------------------------------------------------*/
-
-/*1 ci suall------------------------------------------------------------------------------------------------------------------*/
-
-
-
-// function test(firstname) {
-//     return `salam hormetli ${firstname} `
-// }
-// console.log(test('Leyla'));
-
-
-
-/*2 ci suall------------------------------------------------------------------------------------------------------------------*/
-
-// function number(eded){
-//     if(eded%2==0){
-//         return `${eded} cut ededdir`
-
-
-//     }
-//     else{
-//         return `${eded} tek ededdir`
-//     }
-
-// }
-// console.log(number(3469));
-
-
-
-
-/*4 cu suall------------------------------------------------------------------------------------------------------------------*/
-
-
-// function kub(eded){
-
-//     return eded**3
-// }
-// console.log(kub(3));
-
-/*4 cu suall arrowla------------------------------------------------------------------------------------------------------------------*/
-
-// const kub= (eded) =>eded**3 ;
-// console.log(kub(9));
-
-
-
-/*5 ci suall a varianti-----------------------------------------------------------------------------------------------------------------*/
-// function temperatur(faranheyt) {
-//        return selsi=(faranheyt-32)/1.8
-// }
-// console.log(temperatur(23)); 
-
-
-/*5 ci suall b varianti-----------------------------------------------------------------------------------------------------------------*/
-
-// function temperatur(faranheyt) {
-//     return selsi= 100-(faranheyt-32)/1.8
-// }
-// console.log(temperatur(32)); 
-
-/*5 ci suall birlikde vee alinmadiiiii   :(  :(  :(   :(  :(  :(  :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(*/
-// function temp(qaynama_temperaturu) {
-//     function temperatur(faranheyt){
-//         return selsi=(faranheyt-32)/1.8
-//     }
-//     return temperatur(qaynama_temperaturu)
-// }
-// console.log(temp(4));
-// console.log(temperatur(7));
-
-
-
-/*6 ci suallll  vee alinmadiiiii   :(  :(  :(   :(  :(  :(  :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  :(*/
-// function ters (eded){
-//     const terssetir=eded.toString().reverse();
-//     return terssetir
-// }
-// console.log(ters(7476758));
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~OPERATORS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 /*###############################################################################################################################################*/
 
 
@@ -233,17 +11,17 @@
 
 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^2 ci sual^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
-let num = 100
+// let num = 100
 
-setInterval(() => {
-    num--;
+// setInterval(() => {
+//     num--;
 
-    if (num ==0) {                                   //alinmadii :(   :(  :(  :(  :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  
-        console.log("geri sayim bitti");
+//     if (num ==0) {                                   //alinmadii :(   :(  :(  :(  :(  :(  :(   :(  :(  :(   :(  :(  :(   :(  :(  
+//         console.log("geri sayim bitti");
                                                     
-    }
+//     }
 
-}, 1000)
+// }, 1000)
 
 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^3 cu  sual^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
@@ -272,6 +50,116 @@ setInterval(() => {
 // console.log(date.getSeconds());
 
 
-/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^7 ci sual^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
-const date =new Date('2025,11,12')
-console.log(date.getDay())
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^7 ci sual^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+// const date =new Date('2025,11,12')
+// console.log(date.getDay())
+
+
+/*#######################################################ARRAY #################################################*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++1 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+// let levazimat = ['kitab', 'defter', 'qelem', 'pozan']
+// let newarray=levazimat.splice(2,1,'jurnal','karandas')
+// console.log(newarray);
+
+// 2 ci cehddd 1 ci alinmadi----------------------------------------------------------------------------------
+
+// let levazimat = ['kitab', 'defter', 'qelem', 'pozan']
+// let element =levazimat.indexOf('qelem');
+// levazimat.splice(element,1,'jurnal','karandas')
+// console.log(levazimat);
+
+
+
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++2 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+// let eded =[2,5,32,12,6]
+
+// for (let i=0;i<eded.length;i++) {
+    
+//     if (eded[i]%2==0) {
+//         console.log("eded cut ededdir");
+        
+        
+//     }
+//     else{
+       
+//         console.log("eded tek ededdir")
+//     }
+    
+// }
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++3 cu sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+const array =[-23,5,34,3,-3,0]
+function musbet(eded) {
+    return eded > 0;
+}
+
+
+function vur2(eded) {
+    return eded * 2;
+}
+
+
+
+const result=array.filter(musbet)
+console.log(result);
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++4 cu sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+// let command ="cancel"
+
+// switch (command) {
+//     case 'save':
+//         console.log("yadda saxlandi")
+
+        
+//         break;
+
+//     case 'cancel':
+//         console.log("cixildi")
+//         break;
+
+//     case 'update':
+//         console.log("deyisiklikler yadda saxlandi")
+//         break;
+//     default:
+//         console.log("daxil etdiyiniz command yoxdur")
+//         break;
+// }
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++5 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+// let array=[4,6,2,5,7]
+// let newarray=array.map(i => i%2==0 ?'cut':'tek')
+// console.log(newarray);
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++6 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+// let setir="abcfdeg"
+// let array=setir.split('')
+// console.log(array)
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++7 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+// let object_1={
+//     cloud:'bulud',book:'kitab',juice:'meyve siresi'
+// }
+
+// let object_2={ 
+//     phone:'telefon',window:'pencere'
+// }
+
+// let birlesmis= Object.assign(object_1,object_2);
+// console.log(birlesmis)
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++8 ci sual++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+// let meyve= [ 'alma','armud','heyva','ananas','erik']
+// let array = meyve.split('').includes('a')
+// console.log(array)
+// alinmadi   :(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(:(
